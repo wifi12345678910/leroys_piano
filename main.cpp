6a317206ca39c5c4bfd23551d163327b4607ae2f
Axel, Michael Joseph <maxel3@gatech.edu>
2025-10-13 16:19:17 -0400

    more fix cases

--- main.cpp
+++ main.cpp
@@ -6,14 +6,18 @@
 
 using namespace std;
 
-const tny_uword SETNOTE = 0x9000;
+const tny_uword STORE = 0x9000;
 const tny_uword SETCOLOR = 0x9001;
 const tny_uword DISPKEY = 0x9002;
 const tny_uword PLAYKEY = 0x9003;
 const tny_uword SETTONE = 0x9004;
-const tny_uword GETKEY = 0x9005;
+const tny_uword GETKEY = 0x9006;
 
+tny_uword notes[256];
+tny_uword colors[256];
 
+tny_uword curr_color = 0xFFFF;
+tny_uword curr_note = 0x041;
 
 void bus_read(teenyat *t, tny_uword addr, tny_word *data, uint16_t *delay);
 void bus_write(teenyat *t, tny_uword addr, tny_word data, uint16_t *delay);
@@ -32,11 +36,23 @@ int main(int argc, char *argv[]){
 
 void bus_write(teenyat *t, tny_uword addr, tny_word data, uint16_t *delay) {
     switch(addr) {
-        case SETNOTE:
+        case STORE:
+            notes[int(data.bytes.byte0)] = curr_note;
+            colors[int(data.bytes.byte0)] = curr_color;
+            break;
         case SETCOLOR:
-        case DISPKEY:
-        case PLAYKEY:
+            curr_color = data.u;
+            break;
         case SETTONE:
+            curr_note = data.u;
+        case DISPKEY:
+            light_key(int(data.bytes.byte0));
+            break;
+        case PLAYKEY:
+            play_tone(int(data.bytes.byte0));
+            break;
+        default:
+        break;
     }
 }
 
